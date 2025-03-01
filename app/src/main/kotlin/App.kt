d5d7952f37f230db40c6ad33b69307cba8e04a5e
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2025-03-01 15:33:29 -0300

    init project

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,11 @@
+package br.com.kevin.app
+
+import br.com.kevin.utils.Printer
+
+// This is the main entry point of the application.
+// It uses the `Printer` class from the `:utils` subproject.
+fun main() {
+    val message = "Hello JetBrains!"
+    val printer = Printer(message)
+    printer.printMessage()
+}
